316cea973627f998016eb7fdf5857697ba482243
Tony Collins <tjc@eltham-college.org.uk>
2020-05-28 19:45:19 +0100

    Improved Format

--- PractiseNEA/Program.cs
+++ PractiseNEA/Program.cs
@@ -45,21 +45,26 @@ namespace PractiseNEA
                 // This option should allow the user to type in a size of Taxi either Small, Medium or Large
                 // If a valid size is entered this should be stored in the taxiChoice field of the structure quote.
                 Console.WriteLine("1-Enter Taxi Size?");
+                
+                
                 // This option should allow the user to enter a destination for the trip, it should be between 2 and 20
                 // characters. If a valid destination is entered it should be stored in the destination field of the quote structure.
-
                 Console.WriteLine("2-Enter Destination");
+
                 // This option should allow the user to enter a destination for the trip, it should be between 2 and 20
                 // characters. If a valid destination is entered it should be stored in the destination field of the quote structure.
                 Console.WriteLine("3-Enter start point");
+
                 // This option should allow the user to enter the distance of the taxi journey. It should be betweeen 1 and 50 miles. 
                 // If the distance is valid the value should be stored in the startPoint field of the quote structure.
                 Console.WriteLine("4-Enter distance");
+
                 // This should calculate the price of the journey if Size,destination, starting point and distance have been
                 // entered correctly. The price is equal to the cost of the taxi per mile multiplied by the distance.
                 // The cost of Small, Medium and Large Taxis per mile is stored in the costs text file.
                 // You should print out a quote to console or file and clear the quote structure for the next user.
                 Console.WriteLine("5-Calculate Price and print quote");
+
                 taxiChoice = Console.ReadLine();
 
             } while (taxiChoice != "5");
